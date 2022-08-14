bef027ea5297f6015420891d5f9065772023c892
Jimbanan <uservice371@mail.ru>
2022-08-14 15:14:55 +0300

    Initial commit

---
+++ src/main/java/com/example/vstu_project/VstuProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vstu_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VstuProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VstuProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vstu_project/VstuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vstu_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VstuProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
