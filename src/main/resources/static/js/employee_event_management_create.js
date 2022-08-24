39f0fdff33008e85f57022bb12a3eeb18cf5a1ef
Jimbanan <uservice371@mail.ru>
2022-08-24 17:18:34 +0300

    Made visual design of the "Create a course" page

---
+++ src/main/resources/static/js/employee_event_management_create.js
@@ -0,0 +1,12 @@
+function pageOpenEmployeeMain(url) {
+    console.log(url)
+    window.location.href = "http://localhost:8085/employee/main"
+}
+
+function pageOpenEmployee_lists_of_participants() {
+    window.location.href = "http://localhost:8080/employee/lists_of_participants"
+}
+
+function pageOpenEmployeeEventManagement() {
+    window.location.href = "http://localhost:8080/employee/event_management"
+}
